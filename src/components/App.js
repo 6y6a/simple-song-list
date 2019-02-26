1d6a6e6178da1f84293e4425ed74f641eaf9ec1e
6y6a <a.gubin@leningrad.io>
2019-02-26 23:20:29 +0300

    make App functional

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>Start project</div>
-        )
-    }
+const App = () => {
+    return (
+        <div>Start project</div>
+    )
 }
 
 export default App
