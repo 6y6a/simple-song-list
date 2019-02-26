4e1a963e45b064359daafbcec7a915a42d65dde7
6y6a <gubinalexeymain@gmail.com>
2019-02-27 00:02:43 +0300

    add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+    return [
+        {title: 'No scrumbs', duration: '5:57'},
+        {title: 'Macarena', duration: '3:44'},
+        {title: 'Star Wars', duration: '2:04'},
+        {title: 'Hello kitty', duration: '1:31'},
+    ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+
+    return selectedSong
+}
